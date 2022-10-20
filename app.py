dbc4dbd4bcbac13933ffa051a07c84ac59f5c729
JongBeom Park <jb753858@gmail.com>
2022-10-20 11:28:38 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.wd6li.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+
+    doc = {
+        'num': count,       #몇 번째 버켓리스트인지 구분하기 위함.
+        'bucket': bucket_receive,
+        'done':0        #이 버켓리스트가 완료되었는지 안되었는지 구분하기 위함. 0은 안됨, 1은 완료
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': '등록완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': 'bucket 완료'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+
+    return jsonify({'buckets': bucket_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
